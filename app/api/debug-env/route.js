444901a164361df2906beeda5cc86256249519a9
Nick Janicki <nick.janicki@truerest.com>
2026-05-27 18:43:04 -0700

    remove temp debug endpoint

--- app/api/debug-env/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  const key = process.env.RESEND_API_KEY;
-  return Response.json({
-    hasKey: !!key,
-    keyPrefix: key ? key.slice(0, 8) + '...' : null,
-    keyLength: key ? key.length : 0,
-  });
-}
